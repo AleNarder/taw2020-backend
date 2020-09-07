ae71bf6afd56f5d9a8191dcda1eeb6d97907fd5f
Alessio Narder <alessio.narder@gmail.com>
2020-09-07 08:26:40 +0200

    Fix env

--- src/api/index/routes/index.ts
+++ src/api/index/routes/index.ts
@@ -7,4 +7,12 @@ router
   .route('/')
   .get(status.GET.status)
 
+router
+  .route('/docs/backend')
+  .get(docs.backend)
+
+router
+  .route('/docs/frontend')
+  .get(docs.frontend)
+
 export default router
